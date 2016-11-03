3485aeff13c78316d91a9667132ceab34d23cfc1
xiej <xiej@medlinker.net>
2016-11-03 14:52:47 +0800

    hset 封装完成

--- ssdb/client_test.go
+++ ssdb/client_test.go
@@ -7,54 +7,89 @@ import (
 	"fmt"
 )
 
-func setApi(client Client){
-	client.Set("t1","nil")
+func setApi(client Client) {
+	client.Set("t1", "nil")
 	client.Get("t1")
-	client.Setx("sx","5秒后消失",5)
-	client.Setnx("t1","should not set")
-	client.Expire("t1",5)
-	ttl,_:=client.Ttl("t1")
-	fmt.Println("t1 ttl=",ttl)
-	r,_:=client.Get("t1")
+	client.Setx("sx", "5秒后消失", 5)
+	client.Setnx("t1", "should not set")
+	client.Expire("t1", 5)
+	ttl, _ := client.Ttl("t1")
+	fmt.Println("t1 ttl=", ttl)
+	r, _ := client.Get("t1")
 	fmt.Println(r)
-	old,_:=client.Getset("t1","new")
+	old, _ := client.Getset("t1", "new")
 	fmt.Println(old)
-        client.Del("t1")
-	client.Incr("t1",-2)
-	t1Exists,_:=client.Exists("t2")
-	fmt.Println("t2 is exists?",t1Exists)
-	bit,_:=client.Getbit("t1",-1)
-	fmt.Println("the offset 0 is ",bit)
-	oldbit,_:=client.Setbit("t1",1,1)
-	fmt.Println("the 1 offset bit of t1 is",oldbit)
+	client.Del("t1")
+	client.Incr("t1", -2)
+	t1Exists, _ := client.Exists("t2")
+	fmt.Println("t2 is exists?", t1Exists)
+	bit, _ := client.Getbit("t1", -1)
+	fmt.Println("the offset 0 is ", bit)
+	oldbit, _ := client.Setbit("t1", 1, 1)
+	fmt.Println("the 1 offset bit of t1 is", oldbit)
 
-	client.Set("str","abcdefg")
-	sub,_:=client.Substr("str",1,20)
-	fmt.Println("substr is ",sub)
+	client.Set("str", "abcdefg")
+	sub, _ := client.Substr("str", 1, 20)
+	fmt.Println("substr is ", sub)
 
-	len,_:=client.Strlen("str")
-	fmt.Println("str length is ",len)
+	len, _ := client.Strlen("str")
+	fmt.Println("str length is ", len)
 
-	keys,_:=client.Keys("a","z",10)
-	fmt.Println("get keys array ",keys)
+	keys, _ := client.Keys("a", "z", 10)
+	fmt.Println("get keys array ", keys)
 
-	rkeys,_:=client.RKeys("","",10)
-	fmt.Println("get rkeys array ",rkeys)
+	rkeys, _ := client.RKeys("", "", 10)
+	fmt.Println("get rkeys array ", rkeys)
 
-	maps,_:=client.Scan("","",1000)
-	fmt.Println("get maps ",maps)
+	maps, _ := client.Scan("", "", 1000)
+	fmt.Println("get maps ", maps)
 
-	rmaps,_:=client.Rscan("","",1000)
-	fmt.Println("get rmaps ",rmaps)
+	rmaps, _ := client.Rscan("", "", 1000)
+	fmt.Println("get rmaps ", rmaps)
 
-	client.MultiSet(map[string]string{"red":"#da1337","orange":"#e95a22"})
+	client.MultiSet(map[string]string{"blue":"#da1337", "black":"#e95a22"})
 
-	mget,_:=client.MultiGet([]string{"red","t1","t3"})
-	fmt.Println("multiget result ",mget)
+	mget, _ := client.MultiGet([]string{"red", "t1", "t3"})
+	fmt.Println("multiget result ", mget)
 
-	client.MultiDel([]string{"t1","red"})
+	client.MultiDel([]string{"t1", "red"})
 }
+func hsetApi(client Client) {
+	client.Hset("map1", "m1", "v1");
+	mval, _ := client.Hget("map1", "m1")
+	fmt.Println("value of m1 from map1 is", mval)
+	client.Hdel("map1", "m1")
 
+	oval, _ := client.Hincr("map1", "m1", 10)
+	fmt.Println("after hincr value is ", oval)
+
+	isExists, _ := client.Hexists("map1", "m1")
+	fmt.Println("is m1 in map1 ", isExists)
+
+	mapSize, _ := client.Hsize("map1")
+	fmt.Println("size of map1 is ", mapSize)
+
+	mapNames, _ := client.Hlist("", "", 10)
+	fmt.Println("get map names ", mapNames)
+
+	keys, _ := client.Hkeys("map1", "", "", 10)
+	fmt.Println("get keys from map1", keys)
+
+	allKvs, _ := client.Hgetall("map1")
+	fmt.Println("all kvs of map1", allKvs)
+
+	map1kvs, _ := client.Hscan("map1", "", "", 10)
+	fmt.Println("values of map1", map1kvs)
+
+	count, _ := client.Hclear("map1")
+	fmt.Println("clear maps ", count)
+
+	client.MultiHset("map1", map[string]string{"b1":"1", "b2":"2", "b3":"3"})
+
+	mkvs, _ := client.MultiHget("map1", []string{"b3", "b1"})
+	fmt.Println("multihget ", mkvs)
+	client.MultiHdel("map1", []string{"b1", "a1"})
+}
 func TestSSDBClient_Hscan(t *testing.T) {
 	ssdb := NewSSDBClient("localhost", "8888")
 	err := ssdb.Connect()
@@ -62,6 +97,7 @@ func TestSSDBClient_Hscan(t *testing.T) {
 		t.Error(err)
 	} else {
 		setApi(ssdb)
+		hsetApi(ssdb)
 		//var wait sync.WaitGroup
 		//for i := 0; i < 100; i++ {
 		//	wait.Add(1)
--- ssdb/hset.go
+++ ssdb/hset.go
@@ -1,16 +1,62 @@
 package ssdb
 
-import "strconv"
-
-func (c *Client)Hget(key, value string) (bool, error) {
-	response := c.do("set", key, value)
+func (c *Client)Hset(mapName, key, value string) (bool, error) {
+	response := c.do("hset", mapName, key, value)
 	return response.toBool(), handleResponse(response)
 }
-func (c *Client)Hset(mapName,key, value string) (bool, error) {
-	response := c.do("hset", mapName,key, value)
+func (c *Client)Hget(mapName, key string) (string, error) {
+	response := c.do("hget", mapName, key)
+	return response.toString(), handleResponse(response)
+}
+func (c *Client)Hdel(mapName, key string) (bool, error) {
+	response := c.do("hdel", mapName, key)
 	return response.toBool(), handleResponse(response)
 }
-func (c *Client)Hscan(mapName, startKey,endKey string,limit uint64) (map[string]string, error) {
-	response := c.do("hscan", mapName, startKey,endKey,strconv.FormatUint(limit,10))
+func (c *Client)Hincr(mapName, key string, num int64) (int64, error) {
+	response := c.do("hincr", mapName, key, sInt(num))
+	return response.toInt(), handleResponse(response)
+}
+func (c *Client)Hexists(mapName, key string) (bool, error) {
+	response := c.do("hexists", mapName, key)
+	return response.toBool(), handleResponse(response)
+}
+func (c *Client)Hsize(mapName string) (int64, error) {
+	response := c.do("hsize", mapName)
+	return response.toInt(), handleResponse(response)
+}
+func (c *Client)Hlist(nameStart, nameEnd string, limit int64) ([]string, error) {
+	response := c.do("hlist", nameStart, nameEnd, sInt(limit))
+	return response.toArray(), handleResponse(response)
+}
+func (c *Client)Hrlist(nameStart, nameEnd string, limit int64) ([]string, error) {
+	response := c.do("hrlist", nameStart, nameEnd, sInt(limit))
+	return response.toArray(), handleResponse(response)
+}
+func (c *Client)Hkeys(mapName, nameStart, nameEnd string, limit int64) ([]string, error) {
+	response := c.do("hkeys", mapName, nameStart, nameEnd, sInt(limit))
+	return response.toArray(), handleResponse(response)
+}
+func (c *Client)Hgetall(mapName string) (map[string]string, error) {
+	response := c.do("hgetall", mapName)
 	return response.toMap(), handleResponse(response)
+}
+func (c *Client)Hscan(mapName, nameStart, nameEnd string, limit int64) (map[string]string, error) {
+	response := c.do("hscan",mapName, nameStart, nameEnd, sInt(limit))
+	return response.toMap(), handleResponse(response)
+}
+func (c *Client)Hclear(mapName string) (int64, error) {
+	response := c.do("hclear", mapName)
+	return response.toInt(), handleResponse(response)
+}
+func (c *Client)MultiHset(mapName string, kvs map[string]string) (bool, error) {
+	response := c.do("multi_hset", append([]string{mapName}, sMap(kvs)...)...)
+	return response.toBool(), handleResponse(response)
+}
+func (c *Client)MultiHget(mapName string, keys []string) (map[string]string, error) {
+	response := c.do("multi_hget", append([]string{mapName}, keys...)...)
+	return response.toMap(), handleResponse(response)
+}
+func (c *Client)MultiHdel(mapName string, keys []string) (bool, error) {
+	response := c.do("multi_hdel", append([]string{mapName}, keys...)...)
+	return response.toBool(), handleResponse(response)
 }
